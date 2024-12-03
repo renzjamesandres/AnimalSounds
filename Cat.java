bba1315b89354668363c8f589929cc16d3d01504
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-03 21:41:12 +0800

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,13 @@
+class Cat extends Animal {
+    void eat() {
+        System.out.println("Cats love to eat fish.");
+    }
+
+    void sleep() {
+        System.out.println("Cats sleep for 12-16 hours a day.");
+    }
+
+    void makeSound() {
+        System.out.println("Meow");
+    }
+}
