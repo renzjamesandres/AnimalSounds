bf870a79b58b2a43343145c4dadf9b0e3c530b2f
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-03 21:40:11 +0800

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,13 @@
+class Bird extends Animal {
+    void eat() {
+        System.out.println("Birds love to eat seeds.");
+    }
+
+    void sleep() {
+        System.out.println("Birds sleep for 10-12 hours a day.");
+    }
+
+    void makeSound() {
+        System.out.println("Tweet tweet");
+    }
+}
